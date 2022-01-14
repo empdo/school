597c4c8a10ca227726ca24b3e898eba16c8f2ffc
empdo <emil.essung@live.se>
2022-01-14 17:55:09 +0100

    initial commit

--- tictactoe/src/App.tsx
+++ tictactoe/src/App.tsx
@@ -1,101 +1,80 @@
 import React from 'react';
 import './App.css';
 
-export interface AppProps {}
+function Squares(props: { rows: string[][], placeMarker: (x: number, y: number) => void }) {
+  const { rows, placeMarker } = props;
 
-export interface AppState {
-  rows: Array<String[]>;
-  round: number;
-  winner: string;
+
+  const items = rows.map((row, y) => (<div>
+    {row.map((cell, x) => <span onClick={() => (placeMarker(x, y))}>{cell}</span>)}
+  </div>));
+
+  return <ul>{items}</ul>;
 }
 
-class App extends React.Component<AppProps, AppState>  {
+function calculateWinner(rows: string[][]) {
 
-  constructor(props: AppProps){
-    super(props);
-    this.state = {
-      rows: [[".", ".", "."], [".", ".", "."],[".", ".", "."]],
-      round: 0,
-      winner: "",
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  let squares = [...rows[0], ...rows[1], ...rows[2]];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    console.log(squares);
+    if (squares[a] !== "." && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
   }
 
+  return "";
+}
 
-  calculateWinner(rows: Array<String[]>): string {
 
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
+const App = () => {
+  const [winner, setWinner] = React.useState('');
+  const [rows, setRows] = React.useState([[".", ".", "."], [".", ".", "."], [".", ".", "."]]);
+  const [round, setRound] = React.useState(0);
 
-    let squares = [...rows[0], ...rows[1], ...rows[2]]; 
-    
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      console.log(squares);
-      if (squares[a] !== "." && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return "winner is: " + squares[a];
-      }
+  const restart = () => {
+    setRows([[".", ".", "."], [".", ".", "."], [".", ".", "."]]);
+    setWinner('');
+    setRound(0);
+  }
+
+  const placeMarker = (x: number, y: number) => {
+    if (winner !== '') {
+      return;
     }
 
-    return "";
+    if (rows[y][x] !== ".") {
+      return;
+    }
+
+    const newRows = [...rows];
+    newRows[y][x] = round % 2 === 0 ? "X" : "O";
+    setRows(newRows);
+    setRound(round + 1);
+    setWinner(calculateWinner(newRows));
   }
 
-  updateItem(rowIndex: number, index: number) {
-    this.setState( () => {
-      const rows = this.state.rows;
-
-      if (rows[rowIndex][index] !== "." || this.state.winner !== ""){
-        return;
-      }
-
-      rows[rowIndex][index] = this.state.round % 2 === 0 ? "X" : "O";
-
-      let round = this.state.round + 1;
-
-      const winner =  this.calculateWinner(rows);
-
-      return {
-        rows,
-        round,
-        winner,
-      };
-    });
-  };
-
-  renderSquares(): JSX.Element[] {
-    let list = this.state.rows;
-
-    return list.map((row, index) => {
-      return (
-        <div>
-          <text onClick={() => this.updateItem(index, 0)}>{row[0]}</text>
-          <text onClick={() => this.updateItem(index, 1)}>{row[1]}</text>
-          <text onClick={() => this.updateItem(index, 2)}>{row[2]}</text>
-        </div>
-      )
-    })
-
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-        <text>{this.state.winner}</text>
-        <ul>{this.renderSquares()}</ul>
-        <text onClick={() => this.setState({rows: [[".", ".", "."], [".", ".", "."],[".", ".", "."]], winner: ""})}>restart</text>
-        </header>
-      </div>
-    );
-  }
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        {winner && <p>Player {winner} wins!</p>}
+        <ul><Squares rows={rows} placeMarker={placeMarker} /></ul>
+        <p onClick={restart}>restart</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
